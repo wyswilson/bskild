852e8ef51f0dbb912ce07d86f281c4da63061ba5
Wilson Wong <wyswilson@live.com>
2020-12-21 13:24:22 +1100

    Updated port number

--- api/api.py
+++ api/api.py
@@ -36,5 +36,5 @@ def getskillsbyoccupation(occupation):
 	return func.jsonifyoutput(statuscode,status,"occupations",func.jsonifyskillsbyoccupation(records))
 
 if __name__ == "__main__":
-	app.run(debug=True,host='0.0.0.0',port=99)
-	#waitress.serve(app, host="0.0.0.0", port=99)
+	app.run(debug=True,host='0.0.0.0',port=8888)
+	#waitress.serve(app, host="0.0.0.0", port=8888)
