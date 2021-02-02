fe0a295bd154bddf201d064894df3b70cedede84
Wilson Wong <wyswilson@live.com>
2021-02-03 00:01:27 +1100

    Iterated on webapp

--- webapp/src/App.js
+++ webapp/src/App.js
@@ -1,99 +1,181 @@
 import React from 'react';
 import axios from 'axios';
-import { Dropdown} from 'semantic-ui-react'
-import './App.css';
+import { Dropdown, Header, Grid, Card } from 'semantic-ui-react'
+import './app.css';
 import _ from 'lodash'
+import {isMobile} from 'react-device-detect';
 
 class App extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
       searchendpoint: 'http://bskild.xyz/v1',
-      valueselected: '',
-      valueoptions: []
+      searchquery: '',
+      selectedid: '',
+      selectedtype: '',
+      selectedvalue: '',
+      dropdownoptions: [],
+      rawresponse:[],
+      serp: <Card></Card>
     };
   }
-  updatesuggestions(query,suggestions,type){
+
+  updatesuggestions(suggestions,type){
     const updatedsuggestions = _.map(suggestions, (item) => (
-        {
-          key: item.id,
-          text: item.name,
-          value: item.name,
-          icon: 'user outline' ? type === 'skills' : 'list'
-        }
-      ));
-    console.log(updatedsuggestions);
-    
-    if(type === 'occupations'){
-      this.setState({ valueoptions: updatedsuggestions });
-      this.searchskills(query);
-    }
-    else{
-      this.setState({ 
-        valueoptions: this.state.valueoptions.concat(updatedsuggestions)
-      });
-    }
+      {
+        key: item.id,
+        text: item.name,
+        value: item.name,
+        content: (
+          <Header size='small' icon={type === 'occupations' ? 'user outline'  : 'list'} content={item.name} subheader={item.desc.split(" ").splice(0,20).join(" ") + '...'} />
+        ),
+        type: type
+      }
+    ));
+    this.setState({rawresponse: this.state.rawresponse.concat(suggestions)});
+    this.setState({dropdownoptions: this.state.dropdownoptions.concat(updatedsuggestions)});
   }
 
-  searchskills(query){
+  resetsuggestions(){
+    this.setState({rawresponse: []});
+    this.setState({dropdownoptions: []});
+  }
+
+  async searchskills(query){
+    let suggestions = [];
+
     var skillrequeststr = this.state.searchendpoint + '/skills/' + query
     console.log('search skills [' + skillrequeststr + ']');
-    axios.get(skillrequeststr)
-      .then(response => { 
-        if(response.status === 200){
-          console.log('search skills [' + response.data['message'] + ']');
-          this.updatesuggestions(query,response.data['skills'],"skills");
-        }
-        else{
-          console.log('search skills [' + response.data['message'] + ']');          
-        }
-      })
-      .catch(error => {
-        console.log('search skills [server unreachable]');
-      });
+    try{
+      const response = await axios.get(skillrequeststr);
+      console.log('search skills [' + response.data['message'] + ']');
+      suggestions = response.data['skills'];
+    }
+    catch(err){
+      console.log('search skills [' + err + ']');     
+    }
+    return suggestions;
   }
-  searchoccupations(query){
+
+  async searchoccupations(query){
+    let suggestions = [];
+
     var occupationrequeststr = this.state.searchendpoint + '/occupations/' + query
     console.log('search occupations [' + occupationrequeststr + ']');
-    axios.get(occupationrequeststr)
-      .then(response => { 
-        if(response.status === 200){
-          console.log('search occupations [' + response.data['message'] + ']');
-          this.updatesuggestions(query,response.data['occupations'],"occupations");
-        }
-        else{
-          console.log('search occupations [' + response.data['message'] + ']');          
-        }
-      })
-      .catch(error => {
-        console.log('search occupations [server unreachable]');
-      });
+    try{
+      const response = await axios.get(occupationrequeststr);
+      console.log('search occupations [' + response.data['message'] + ']');
+      suggestions = response.data['occupations'];
+    }
+    catch(err){
+      console.log('search occupations [' + err + ']');     
+    }
+    return suggestions;
   }
+
+  async searchboth(query){
+    await this.resetsuggestions();
+    const options1 = await this.searchoccupations(query);
+    await this.updatesuggestions(options1,'occupations');
+    const options2 = await this.searchskills(query);
+    await this.updatesuggestions(options2,'skills');
+    await this.refreshresults();
+  }
+
   searchkeywords(event, data){
     const keywords = data.searchQuery;
-
+    this.setState({searchquery: keywords});
     if(keywords.length > 3){
-      this.searchoccupations(keywords);
+      this.searchboth(keywords);
     }
   }
-  dosomething(event, data){
-    const field = data.name;
-    const value = data.value;
+
+  async setvalue(event, data){
+    const fieldname = data.name;
+    if(fieldname === 'keywords'){
+      const keywords = data.value;
+      this.setState({searchquery: keywords});
+      const suggestions = this.state.dropdownoptions;
+
+      let selectedarr = suggestions.filter(suggest => suggest.value.includes(keywords))[0];
+      const selectedid = selectedarr.key;
+      const selectedtype = selectedarr.type;
+      const selectedvalue = selectedarr.value;
+      console.log("selected [" + selectedtype + "][" + selectedid + "][" + selectedvalue + "]");
+      this.setState({selectedid: selectedid});
+      this.setState({selectedtype: selectedtype});
+      this.setState({selectedvalue: selectedvalue});
+
+      if(selectedtype === 'skills'){
+        const options = await this.searchskills(selectedid);
+        await this.resetsuggestions();
+        await this.updatesuggestions(options,'skills');
+        await this.refreshresults();
+      }
+      else if(selectedtype === 'occupations'){
+        const options = await this.searchoccupations(selectedid);
+        await this.resetsuggestions();
+        await this.updatesuggestions(options,'occupations');
+        await this.refreshresults();
+      }
+    }
   }
+
+  refreshresults(){
+    console.log("refreshing results");
+    const serprefreshed = this.state.rawresponse.map( (item) => (
+         <Card key={item.id}>
+          <Card.Content>
+            <Card.Header>{item.name}</Card.Header>
+            <Card.Meta>
+              {item.type ? item.type + ', ' + item.reusability : ''}
+            </Card.Meta>
+            <Card.Description>
+              {item.desc}
+            </Card.Description>
+          </Card.Content>
+          <Card.Content extra>
+          </Card.Content>
+        </Card>
+           ));
+    console.log(serprefreshed);
+    this.setState({serp: serprefreshed});
+  }
+
+  handleenter(event){
+    if(event.code === 'Enter'){
+      console.log("auto suggest not used");
+      this.searchboth(this.state.searchquery);
+    }
+  }
+
   render() {
     return (
-      <div>
-
-        <Dropdown name="keywords"
-            search
-            selection
-            value={this.state.valueselected}
-            options={this.state.valueoptions}
-            noResultsMessage = "No results found"
-            onSearchChange={this.searchkeywords.bind(this)}
-            onChange={this.dosomething.bind(this)}
-        />
-
+      <div
+        className={isMobile ? "bodymain mobile" : "bodymain"}
+      >
+        <Grid stackable columns={1}>
+          <Grid.Column>
+            <Dropdown name="keywords" className="fullwidth"
+                search clearable compact
+                selection
+                searchQuery={this.state.searchquery}
+                value={this.state.searchquery}
+                options={this.state.dropdownoptions}
+                noResultsMessage = "No results found"
+                onSearchChange={this.searchkeywords.bind(this)}
+                onChange={this.setvalue.bind(this)}
+                //onKeyDown={this.handleenter.bind(this)}
+                placeholder='Occupation or skill'
+            />
+          </Grid.Column>
+          <Grid.Column>
+            <Card.Group>
+            {this.state.serp}
+            </Card.Group>
+          </Grid.Column>
+        
+        </Grid>
       </div>
     )
   }
