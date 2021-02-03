3fde1487895103bfcf57c1544145fc803551aed4
Wilson Wong <wyswilson@live.com>
2021-02-03 11:39:55 +1100

    Completed v1 of webapp

--- webapp/src/App.js
+++ webapp/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import axios from 'axios';
-import { Dropdown, Header, Grid, Card } from 'semantic-ui-react'
+import { Popup, Label, Icon, Dropdown, Header, Grid, Card } from 'semantic-ui-react'
 import './app.css';
 import _ from 'lodash'
 import {isMobile} from 'react-device-detect';
@@ -16,35 +16,14 @@ class App extends React.Component {
       selectedvalue: '',
       dropdownoptions: [],
       rawresponse:[],
-      serp: <Card></Card>
+      serp:''
     };
-  }
-
-  updatesuggestions(suggestions,type){
-    const updatedsuggestions = _.map(suggestions, (item) => (
-      {
-        key: item.id,
-        text: item.name,
-        value: item.name,
-        content: (
-          <Header size='small' icon={type === 'occupations' ? 'user outline'  : 'list'} content={item.name} subheader={item.desc.split(" ").splice(0,20).join(" ") + '...'} />
-        ),
-        type: type
-      }
-    ));
-    this.setState({rawresponse: this.state.rawresponse.concat(suggestions)});
-    this.setState({dropdownoptions: this.state.dropdownoptions.concat(updatedsuggestions)});
-  }
-
-  resetsuggestions(){
-    this.setState({rawresponse: []});
-    this.setState({dropdownoptions: []});
   }
 
   async searchskills(query){
     let suggestions = [];
 
-    var skillrequeststr = this.state.searchendpoint + '/skills/' + query
+    var skillrequeststr = this.state.searchendpoint + '/skills/' + query + '?lite'
     console.log('search skills [' + skillrequeststr + ']');
     try{
       const response = await axios.get(skillrequeststr);
@@ -60,7 +39,7 @@ class App extends React.Component {
   async searchoccupations(query){
     let suggestions = [];
 
-    var occupationrequeststr = this.state.searchendpoint + '/occupations/' + query
+    var occupationrequeststr = this.state.searchendpoint + '/occupations/' + query + '?lite'
     console.log('search occupations [' + occupationrequeststr + ']');
     try{
       const response = await axios.get(occupationrequeststr);
@@ -74,7 +53,6 @@ class App extends React.Component {
   }
 
   async searchboth(query){
-    await this.resetsuggestions();
     const options1 = await this.searchoccupations(query);
     await this.updatesuggestions(options1,'occupations');
     const options2 = await this.searchskills(query);
@@ -83,14 +61,39 @@ class App extends React.Component {
   }
 
   searchkeywords(event, data){
+    this.resetsuggestions();
     const keywords = data.searchQuery;
+    console.log("searchkeywords [" + keywords + "]");
     this.setState({searchquery: keywords});
     if(keywords.length > 3){
       this.searchboth(keywords);
     }
   }
 
-  async setvalue(event, data){
+  updatesuggestions(suggestions,type){
+    const updatedsuggestions = _.map(suggestions, (item) => (
+      {
+        key: item.id,
+        text: item.name,
+        value: item.name,
+        desc: item.desc,
+        content: (
+          <Header size='small' color={type === 'occupations' ? 'blue'  : 'orange'} icon={type === 'occupations' ? 'user outline'  : 'list'} content={item.name} subheader={item.desc.split(" ").splice(0,20).join(" ") + '...'} />
+        ),
+        type: type
+      }
+    ));
+    this.setState({rawresponse: this.state.rawresponse.concat(suggestions)});
+    this.setState({dropdownoptions: this.state.dropdownoptions.concat(updatedsuggestions)});
+  }
+
+  resetsuggestions(){
+    this.setState({rawresponse: []});
+    this.setState({dropdownoptions: []});
+    this.setState({serp: ''});
+  }
+
+  async selectsuggestion(event, data){
     const fieldname = data.name;
     if(fieldname === 'keywords'){
       const keywords = data.value;
@@ -102,33 +105,49 @@ class App extends React.Component {
       const selectedtype = selectedarr.type;
       const selectedvalue = selectedarr.value;
       console.log("selected [" + selectedtype + "][" + selectedid + "][" + selectedvalue + "]");
-      this.setState({selectedid: selectedid});
-      this.setState({selectedtype: selectedtype});
-      this.setState({selectedvalue: selectedvalue});
 
-      if(selectedtype === 'skills'){
-        const options = await this.searchskills(selectedid);
-        await this.resetsuggestions();
-        await this.updatesuggestions(options,'skills');
-        await this.refreshresults();
-      }
-      else if(selectedtype === 'occupations'){
-        const options = await this.searchoccupations(selectedid);
-        await this.resetsuggestions();
-        await this.updatesuggestions(options,'occupations');
-        await this.refreshresults();
-      }
+      this.suggestionselected(selectedtype,selectedid,selectedvalue);
+    }
+  }
+
+  async suggestionselected(type,id,value){
+    console.log(type + "-" + id);
+    this.setState({selectedid: id});
+    this.setState({selectedtype: type});
+    this.setState({selectedvalue: value});
+    this.setState({searchquery: value});
+
+    if(type === 'skills'){
+      const options = await this.searchskills(id);
+      await this.resetsuggestions();
+      await this.updatesuggestions(options,'skills');
+      await this.refreshresults();
+    }
+    else if(type === 'occupations'){
+      const options = await this.searchoccupations(id);
+      await this.resetsuggestions();
+      await this.updatesuggestions(options,'occupations');
+      await this.refreshresults();
     }
   }
 
   refreshresults(){
     console.log("refreshing results");
-    const serprefreshed = this.state.rawresponse.map( (item) => (
-         <Card key={item.id}>
+    const serprefreshed = this.state.dropdownoptions.map( (item) => (
+         <Card key={item.key}
+          color={item.type === 'occupations' ? 'blue'  : 'orange'}
+          onClick={this.suggestionselected.bind(this,item.type,item.key,item.value)}
+         >
           <Card.Content>
-            <Card.Header>{item.name}</Card.Header>
+            <Label corner='right'>
+              <Icon
+                name={item.type === 'occupations' ? 'user outline'  : 'list'}
+                color={item.type === 'occupations' ? 'blue'  : 'orange'}
+              />
+            </Label>
+            <Card.Header>{item.value}</Card.Header>
             <Card.Meta>
-              {item.type ? item.type + ', ' + item.reusability : ''}
+              {item.type}
             </Card.Meta>
             <Card.Description>
               {item.desc}
@@ -142,14 +161,14 @@ class App extends React.Component {
     this.setState({serp: serprefreshed});
   }
 
-  handleenter(event){
-    if(event.code === 'Enter'){
-      console.log("auto suggest not used");
-      this.searchboth(this.state.searchquery);
-    }
-  }
-
   render() {
+    let results = ''; 
+    if(this.state.serp !== ''){
+      results = (
+        <Card.Group>{this.state.serp}</Card.Group>
+      )
+    }
+
     return (
       <div
         className={isMobile ? "bodymain mobile" : "bodymain"}
@@ -157,24 +176,24 @@ class App extends React.Component {
         <Grid stackable columns={1}>
           <Grid.Column>
             <Dropdown name="keywords" fluid
-                search clearable compact
-                selection
+                search compact
+                selection allowAdditions
+                additionLabel='Search with '
+                minCharacters={3}
+                selectOnBlur={false}
                 searchQuery={this.state.searchquery}
                 value={this.state.searchquery}
                 options={this.state.dropdownoptions}
                 noResultsMessage = "No results found"
                 onSearchChange={this.searchkeywords.bind(this)}
-                onChange={this.setvalue.bind(this)}
-                //onKeyDown={this.handleenter.bind(this)}
+                onChange={this.selectsuggestion.bind(this)}
                 placeholder='Occupation or skill'
+                onAddItem={this.searchkeywords.bind(this)}
             />
           </Grid.Column>
           <Grid.Column>
-            <Card.Group>
-            {this.state.serp}
-            </Card.Group>
+            {results}
           </Grid.Column>
-        
         </Grid>
       </div>
     )
