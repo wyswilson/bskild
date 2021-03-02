6bb2a95367bf959c4358bd65362c9ad0c61bcb13
Wilson Wong <wyswilson@live.com>
2021-03-02 11:46:18 +1100

    Added retrieval of faved occupations by user

--- api/api.py
+++ api/api.py
@@ -303,5 +303,5 @@ def getoccupationsbyskills(skill):
 
 
 if __name__ == "__main__":
-	app.run(debug=True,host='0.0.0.0',port=8888)
-	#waitress.serve(app, host="0.0.0.0", port=8888)
+	#app.run(debug=True,host='0.0.0.0',port=8888)
+	waitress.serve(app, host="0.0.0.0", port=8888)
